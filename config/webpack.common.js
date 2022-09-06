5f1dafc1bd503e8b9d909c44cf01c32fade6a92a
terracotta <begaiym.akylova@gmail.com>
2022-09-06 13:15:12 +0600

    Add dependencies and structure

---
+++ config/webpack.common.js
@@ -0,0 +1,75 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  assets: "assets/",
+};
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((filename) => filename.endsWith(".pug"));
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: PATHS.src,
+  },
+  output: {
+    path: PATHS.dist,
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "../dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "~": PATHS.src,
+      "@": `${PATHS.src}/js`,
+    },
+  },
+  plugins: [
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}${page}`,
+          filename: `./${page.replace(/\.pug/, ".html")}`,
+        })
+    ),
+  ],
+};
